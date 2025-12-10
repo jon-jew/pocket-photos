6a64b944cb596fd4579aae9605f96b749c0289dd
Jonathan Jew <32943817+jon-jew@users.noreply.github.com>
2025-12-09 20:09:04 -0500

    Disable waitlist message useEffect
    Comment out useEffect for waitlist message handling.

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -61,14 +61,14 @@ export default function Home({ initialUser }: { initialUser: UserInfo | undefine
     }
   }
 
-  useEffect(() => {
-    if (fromWaitlist) {
-      toast.success('Thank you for signing up!');
-    }
-    timeoutRef.current = setTimeout(() => {
-      setWaitlistMsgOpen(true);
-    }, 1000);
-  }, []);
+  // useEffect(() => {
+  //   if (fromWaitlist) {
+  //     toast.success('Thank you for signing up!');
+  //   }
+  //   timeoutRef.current = setTimeout(() => {
+  //     setWaitlistMsgOpen(true);
+  //   }, 1000);
+  // }, []);
 
   return (
     <div className="flex flex-col min-h-screen items-center justify-center">
@@ -102,6 +102,7 @@ export default function Home({ initialUser }: { initialUser: UserInfo | undefine
           <p>42 Clinton Place</p>
           <p>Hackensack, NJ 07601</p>
           <p>(732) 979-4563</p>
+          <p>dev@plurr.io</p>
         </div>
       </TornContainer>
       <Snackbar
